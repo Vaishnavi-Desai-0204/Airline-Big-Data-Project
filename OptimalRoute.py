7f867875efa9b0ba04a1626cec74403289a3923a
Vaishnavi Desai <78345767+Vaishnavi-Desai-0204@users.noreply.github.com>
2023-04-06 15:45:18 -0700

    Update OptimalRoute.py

--- OptimalRoute.py
+++ OptimalRoute.py
@@ -13,40 +13,19 @@ def haversine(lat1, lon1, lat2, lon2):
     d = R * c # Distance in km
     return d
 
-import heapq
-
 def dijkstra(source_airport, dest_airport):
-    # Initialize distance and visited dictionaries
+    SLAT = (airports_df.loc[airports_df['IATA'] == source_airport, 'Latitude'].values[0])
+    SLONG = (airports_df.loc[airports_df['IATA'] == source_airport, 'Longitude'].values[0])
     distances = {source_airport: 0}
-    visited = {}
-
-    # Initialize priority queue with source airport
-    pq = [(0, source_airport)]
-
-    while pq:
-        # Get the closest airport from the priority queue
-        (dist, curr_airport) = heapq.heappop(pq)
-
-        # Check if we've found the destination airport
-        if curr_airport == dest_airport:
-            return dist
-
-        # Mark the current airport as visited
-        visited[curr_airport] = True
-
-        # Update the distances of neighboring airports
-        for index, row in routes_df[routes_df['Source airport'] == curr_airport].iterrows():
-            neighbor_airport = row['Destination airport']
-            neighbor_distance = row['Distance']
-            if neighbor_airport not in visited:
-                new_distance = dist + neighbor_distance
-                if neighbor_airport not in distances or new_distance < distances[neighbor_airport]:
-                    distances[neighbor_airport] = new_distance
-                    heapq.heappush(pq, (new_distance, neighbor_airport))
-
-    # If we get here, there is no path from source to dest
-    return float('inf')
-
+    
+    for index, row in routes_df[routes_df['Source airport'] == source_airport].iterrows():
+        neighbor_airport = row['Destination airport']
+        DLAT = (airports_df.loc[airports_df['IATA'] == neighbor_airport, 'Latitude'].values[0])
+        DLONG = (airports_df.loc[airports_df['IATA'] == neighbor_airport, 'Longitude'].values[0])
+        distance = haversine(SLAT,SLONG,DLAT,DLONG)
+        distances[neighbor_airport] = distance
+        
+    print(distances)
 
 # Load the data
 airports_df = pd.read_csv('airports.dat', header=None, names=['Airport ID', 'Name', 'City', 'Country', 'IATA', 'ICAO', 'Latitude', 'Longitude', 'Altitude', 'Timezone', 'DST', 'Tz database time zone', 'Type', 'Source'], index_col=0)
